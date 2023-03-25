3f58c62a71849ac4eae23311d0fc0669cb653818
KoheiShibata <ko.bsk116@gmail.com>
2023-03-25 23:05:23 +0900

    #1 タイマー機能修正

--- typing.js
+++ typing.js
@@ -1,23 +1,18 @@
-let failCount = 0;
-let successCount = 0;
-let startTime = 3;
-let timeLimit = 30;
 const main = document.getElementById("main")
 const textWrap = document.getElementById("typeText")
 const startButton = document.getElementById("startButton")
 const fail = document.getElementById("failCount")
 const success = document.getElementById("successCount")
 const remainingTime = document.getElementById("timeLimit")
+const startWrap = document.getElementById("startWrap")
+
+let startTime = 3;
+let timeLimit = 10;
+let successCount = 0;
+let failCount = 0;
 
 const question = ["Songoku", "Chi-Chi", "Nappa", "Vegeta", "Raditz", "Broly", "Freeza", "Zarbon", "Dodoria", "Ginyu-Forces", "Kurilllin", "Piccolo", "Yamcha", "Trunks"];
 let checkText = [];
-
-
-function countDown() {
-    startButton.textContent = startTime
-    startTime--;
-}
-
 
 // 問題を作成
 function createText() {
@@ -34,31 +29,53 @@ function createText() {
 
 // スタートタイマー
 startButton.addEventListener("click", () => {
+    const countDown = () => {
+        startButton.textContent = startTime
+        startTime--;
+    }
     const timer = setInterval(()=> {
         countDown()
         if(startTime < 0) {
             clearInterval(timer)
-            createText()
-            main.style.display = "block"
-            typeTimer()
+            typeDisplay()
+            startWrap.style.display = "none"
         }
     }, 1000)
 })
 
-// 制限時間
-function typeTimer() {
-    const countDown = () => {
-        remainingTime.textContent = timeLimit
-        timeLimit--;
-    }
-    const typeTimer = setInterval(() => {
-        countDown()
-        if (timeLimit < 0) {
-            clearInterval(typeTimer)
-        }
-    }, 1000) 
+// タイピングゲーム表示
+function typeDisplay() {
+    startTime = 3;
+    timeLimit = 10;
+    failCount = 0;
+    successCount = 0;
+    textWrap.classList.remove("fail-color")
+    fail.innerHTML = failCount
+    success.innerHTML = successCount
+    remainingTime.textContent = timeLimit
+    main.style.display = "block"
+    createText()
+    typeTimer()
 }
 
+// タイプタイマー
+function typeTimer() {
+    let startTime = performance.now();
+    
+    const animate = (now) => {
+        let elapsed = Math.floor((now - startTime) / 1000);
+      remainingTime.textContent = timeLimit - elapsed;
+      
+      if (elapsed < timeLimit) {
+        requestAnimationFrame(animate);
+      } else {
+        main.style.display = "none";
+        startButton.textContent = "Restart";
+        startWrap.style.display = "block";
+      }
+    };
+    requestAnimationFrame(animate);
+  }
 
 // タイピング処理
 window.addEventListener("keydown", function (e) {
